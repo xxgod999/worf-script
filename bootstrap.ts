a77a000b8300faf7e3c1a452c00c9ec0a4086311
xxgod999 <abrainer591@gmail.com>
2026-06-02 21:38:27 +0300

    Fix bootstrap - just load index.ts

--- bootstrap.ts
+++ bootstrap.ts
@@ -1,13 +1,2 @@
-import { EventsSDK } from "github.com/octarine-public/wrapper/index"
-
-import { MainManager } from "./Manager/Main"
-import { MenuManager } from "./Manager/Menu"
-
-const IMenu = new MenuManager()
-const IManager = new MainManager(IMenu)
-
-EventsSDK.on("Tick", (dt: number) => IManager.OnTick(dt))
-EventsSDK.on("GameEnded", () => IManager.OnGameEnded())
-EventsSDK.on("GameStarted", () => IManager.OnGameStarted())
-EventsSDK.on("EntityCreated", (entity) => IManager.OnEntityCreated(entity))
-EventsSDK.on("EntityDestroyed", (entity) => IManager.OnEntityDestroyed(entity))
+// Bootstrap entry point - loads the main script
+import "./index"
